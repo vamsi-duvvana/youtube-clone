a2105027bb002d1cc0103dda54f85e07388f16d8
Vamsi Naidu <vamsi2.naidu@tataaig.com>
2024-01-28 02:00:24 +0530

    comments container

---
+++ src/components/CommentsContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const commentsContainer = () => {
+  return (
+    <div>commentsContainer</div>
+  )
+}
+
+export default commentsContainer
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { closeMenu } from '../utils/configSlice';
 import { useSearchParams } from 'react-router-dom';
+import CommentsContainer from './CommentsContainer'
 
 const WatchPage = () => {
 
@@ -15,17 +16,20 @@ const WatchPage = () => {
     }, [])
 
     return (
-        <div className='px-5'>
-            <iframe
-                width="1100"
-                height="550"
-                src={`https://www.youtube.com/embed/${searchParams.get("v")}?si=3iVFPvNyepj9tYNk`}
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
-                allowFullScreen>
-            </iframe>
-        </div>
+        <>
+            <div className='px-5'>
+                <iframe
+                    width="1100"
+                    height="550"
+                    src={`https://www.youtube.com/embed/${searchParams.get("v")}?si=3iVFPvNyepj9tYNk`}
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+                    allowFullScreen>
+                </iframe>
+            </div>
+            <CommentsContainer />
+        </>
     )
 }
 
